e65d55bbfff34020a30e0d07b0fd069291906a8a
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-05-12 22:18:34 +0530

    added the config folder

---
+++ src/main/java/com/springboot/jwt/refresh/token/config/AppSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.springboot.jwt.refresh.token.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class AppSecurityConfig {
+	
+	
+
+}
