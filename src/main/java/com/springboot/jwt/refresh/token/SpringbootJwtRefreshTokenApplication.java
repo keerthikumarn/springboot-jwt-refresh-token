5ee1b5b490287eca2b2ac671dad69f7328f833cc
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-05-12 16:47:20 +0530

    first commit

---
+++ src/main/java/com/springboot/jwt/refresh/token/SpringbootJwtRefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jwt.refresh.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtRefreshTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtRefreshTokenApplication.class, args);
+	}
+
+}
