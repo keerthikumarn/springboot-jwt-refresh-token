546b8ee50ffd6ef8dcd635fbf86ed165c4870655
Keerthi Kumar Narayan <keerthi.narayan@5628L11-ux.subexworld.org>
2024-05-13 16:59:10 +0530

    started with the loading of employee details

--- src/main/java/com/springboot/jwt/refresh/token/dto/Employee.java
+++ src/main/java/com/springboot/jwt/refresh/token/dto/Employee.java
@@ -1,12 +1,14 @@
 package com.springboot.jwt.refresh.token.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Employee {
 
 	private int empNo;
--- src/main/java/com/springboot/jwt/refresh/token/service/EmployeeService.java
+++ src/main/java/com/springboot/jwt/refresh/token/service/EmployeeService.java
@@ -1,13 +1,19 @@
 package com.springboot.jwt.refresh.token.service;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.springboot.jwt.refresh.token.dto.Employee;
+import com.springboot.jwt.refresh.token.entity.UserInfo;
 import com.springboot.jwt.refresh.token.repo.UserInfoRepository;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class EmployeeService {
@@ -30,4 +36,23 @@ public class EmployeeService {
                 .findAny()
                 .orElseThrow(() -> new RuntimeException("Employee with id: " + id + " not found"));
 	}
+	
+	public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        userInfoRepo.save(userInfo);
+        return "user added to system ";
+    }
+	
+	@PostConstruct
+    public void loadEmployeesFromDB() {
+        empList = IntStream.rangeClosed(1, 100).mapToObj(index -> Employee.builder().empNo(index)
+        		.name("user_"+index).designation("tech arch").build()).collect(Collectors.toList());
+        
+        empList = IntStream.rangeClosed(1, 100)
+                .mapToObj(index -> Product.builder()
+                        .empNo(index)
+                        .name("User_ " + index)
+                        .designation("tech architect")).build()
+                ).collect(Collectors.toList());
+    }
 }
