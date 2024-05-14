ceedb439a35efb9dfd31ae843d10c50d63f623fe
Keerthi Kumar Narayan <keerthi.narayan@5628L11-ux.subexworld.org>
2024-05-14 11:19:45 +0530

    Finished the refresh token service implementation

--- src/main/java/com/springboot/jwt/refresh/token/service/RefreshTokenService.java
+++ src/main/java/com/springboot/jwt/refresh/token/service/RefreshTokenService.java
@@ -1,6 +1,7 @@
 package com.springboot.jwt.refresh.token.service;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +16,27 @@ public class RefreshTokenService {
 
 	@Autowired
 	private RefreshTokenRepository refreshTokenRepository;
-	
+
 	@Autowired
 	private UserInfoRepository userInfoRepository;
-	
+
 	public RefreshToken createRefreshToken(String username) {
-		RefreshToken refreshToken = RefreshToken.builder()
-				.userInfo(userInfoRepository.findByName(username).get())
-                .token(UUID.randomUUID().toString())
-                .expiryDate(Instant.now().plusMillis(600000))//10
-                .build();
-        return refreshTokenRepository.save(refreshToken);
+		RefreshToken refreshToken = RefreshToken.builder().userInfo(userInfoRepository.findByName(username).get())
+				.token(UUID.randomUUID().toString()).expiryDate(Instant.now().plusMillis(600000)).build();
+		return refreshTokenRepository.save(refreshToken);
 		return null;
 	}
-	
-	
+
+	public Optional<RefreshToken> findByToken(String token) {
+		return refreshTokenRepository.findByToken(token);
+	}
+
+	public RefreshToken verifyExpiration(RefreshToken token) {
+		if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+			refreshTokenRepository.delete(token);
+			throw new RuntimeException(
+					token.getToken() + " Refresh token has been expired. Please make a new signin request");
+		}
+		return token;
+	}
 }
