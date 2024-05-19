82f49aa854c6cfc82e733799336f19c9f888443a
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-05-19 14:03:04 +0530

    implemented getEmployeeId and getEmployees

--- src/main/java/com/springboot/jwt/refresh/token/controller/EmployeeController.java
+++ src/main/java/com/springboot/jwt/refresh/token/controller/EmployeeController.java
@@ -1,12 +1,18 @@
 package com.springboot.jwt.refresh.token.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.jwt.refresh.token.dto.Employee;
 import com.springboot.jwt.refresh.token.entity.EmployeeInfo;
 import com.springboot.jwt.refresh.token.service.EmployeeService;
 import com.springboot.jwt.refresh.token.service.JWTService;
@@ -31,6 +37,18 @@ public class EmployeeController {
     @PostMapping("/signUp")
     public String addNewEmployee(@RequestBody EmployeeInfo empInfo) {
     	return empService.addNewEmployee(empInfo);
+    }
+    
+    @GetMapping("all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Employee> getAllEmployees() {
+    	return empService.getEmpList();
+    }
+    
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Employee getEmployeeById(@PathVariable int id) {
+    	return empService.getEmployee(id);
     }
     
 }
