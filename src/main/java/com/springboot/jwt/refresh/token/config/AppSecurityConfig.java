b386d8712788942745e670a69c9003e19e2141a8
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-05-19 13:36:56 +0530

    started with AppSecurityConfig class

--- src/main/java/com/springboot/jwt/refresh/token/config/AppSecurityConfig.java
+++ src/main/java/com/springboot/jwt/refresh/token/config/AppSecurityConfig.java
@@ -3,9 +3,15 @@ package com.springboot.jwt.refresh.token.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.springboot.jwt.refresh.token.filter.JWTAuthFilter;
 
@@ -21,5 +27,25 @@ public class AppSecurityConfig {
 	public UserDetailsService userDetailsService() {
         return new UserInfoUserDetailsService();
     }
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+		daoAuthProvider.setUserDetailsService(userDetailsService());
+		daoAuthProvider.setPasswordEncoder(passwordEncoder());
+		return daoAuthProvider;
+	}
+	
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+	
+	
 
 }
